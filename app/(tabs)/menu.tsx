85564cbd4c582593e88f6128a769962e21a82ef7
Nikoloz97 <nick.gotsy@gmail.com>
2025-06-19 12:15:40 -0400

    Menu: updated menuItems to better align with real content; views are now buttons

--- app/(tabs)/menu.tsx
+++ app/(tabs)/menu.tsx
@@ -2,28 +2,24 @@ import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { Image } from "expo-image";
 import React from "react";
-import { ScrollView, TouchableOpacity } from "react-native";
+import { Platform, ScrollView, TouchableOpacity } from "react-native";
 
 const menuItems = [
   {
-    name: "Hot Coffee",
-    image: "https://placehold.co/100x100/coffee?text=Hot+Coffee",
+    name: "Shakes",
+    image: "https://placehold.co/100x100/coffee?text=Shakes",
   },
   {
-    name: "Cold Coffee",
-    image: "https://placehold.co/100x100/iced?text=Cold+Coffee",
+    name: "Smoothies",
+    image: "https://placehold.co/100x100/iced?text=Smoothies",
   },
   {
-    name: "Hot Tea",
-    image: "https://placehold.co/100x100/tea?text=Hot+Tea",
+    name: "Beverage",
+    image: "https://placehold.co/100x100/tea?text=Beverages",
   },
   {
-    name: "Cold Tea",
-    image: "https://placehold.co/100x100/greentea?text=Cold+Tea",
-  },
-  {
-    name: "Refreshers",
-    image: "https://placehold.co/100x100/red?text=Refreshers",
+    name: "Desserts",
+    image: "https://placehold.co/100x100/red?text=Desserts",
   },
 ];
 
@@ -35,51 +31,69 @@ export default function Menu() {
         type="card"
         style={{
           flexDirection: "row",
-          justifyContent: "space-between",
           alignItems: "center",
-          paddingHorizontal: 15,
+          justifyContent: "center",
           paddingVertical: 20,
+          borderRadius: 0,
+          padding: 0,
+          marginBottom: 0,
+          ...Platform.select({
+            ios: {
+              shadowColor: "#000",
+              shadowOffset: { width: 0, height: 4 },
+              shadowOpacity: 0.2,
+              shadowRadius: 6,
+            },
+            android: {
+              elevation: 8, // shadow elevation
+            },
+          }),
         }}
       >
-        <ThemedText type="title">Drinks</ThemedText>
-        <TouchableOpacity>
-          <ThemedText style={{ color: "#006241", fontWeight: "bold" }}>
-            See all 121
-          </ThemedText>
-        </TouchableOpacity>
+        <ThemedText
+          type="title"
+          style={{
+            paddingTop: 40,
+          }}
+        >
+          Menu
+        </ThemedText>
       </ThemedView>
 
       {/* Menu list */}
       <ScrollView
         contentContainerStyle={{
           paddingHorizontal: 15,
+          paddingTop: 10,
+
           paddingBottom: 40,
         }}
       >
         {menuItems.map((item, index) => (
-          <ThemedView
-            key={index}
-            type="card"
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              marginBottom: 15,
-              padding: 10,
-              borderRadius: 12,
-            }}
-          >
-            <Image
-              source={{ uri: item.image }}
+          <TouchableOpacity key={index}>
+            <ThemedView
+              type="card"
               style={{
-                width: 60,
-                height: 60,
-                borderRadius: 30,
-                marginRight: 15,
+                flexDirection: "row",
+                alignItems: "center",
+                marginBottom: 15,
+                padding: 10,
+                borderRadius: 12,
               }}
-              contentFit="cover"
-            />
-            <ThemedText style={{ fontSize: 18 }}>{item.name}</ThemedText>
-          </ThemedView>
+            >
+              <Image
+                source={{ uri: item.image }}
+                style={{
+                  width: 60,
+                  height: 60,
+                  borderRadius: 30,
+                  marginRight: 15,
+                }}
+                contentFit="cover"
+              />
+              <ThemedText style={{ fontSize: 18 }}>{item.name}</ThemedText>
+            </ThemedView>
+          </TouchableOpacity>
         ))}
       </ScrollView>
     </ThemedView>
