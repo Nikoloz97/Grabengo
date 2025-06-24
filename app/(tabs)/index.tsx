61b15762b3d6935c786ee040cdfc6f94f83270c9
Nikoloz97 <nick.gotsy@gmail.com>
2025-06-23 21:19:59 -0400

    Created home content constant; redesigned home ui to use constant

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,11 @@
-import { ThemedButton } from "@/components/themed-button";
 import { ThemedHeaderView } from "@/components/themed-header-view";
 import { ThemedScrollView } from "@/components/themed-scroll-view";
 import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
+import { homeContent } from "@/constants/home-content";
 import { Image } from "expo-image";
 import React from "react";
+import { View } from "react-native";
 
 export default function HomeScreen() {
   return (
@@ -14,100 +15,62 @@ export default function HomeScreen() {
         <ThemedText type="title">Grabengo</ThemedText>
       </ThemedHeaderView>
       {/* main */}
-      <ThemedScrollView>
-        {/* featured */}
-        <ThemedView type="card">
-          <ThemedText
+      <ThemedScrollView
+        style={{
+          flex: 1,
+          alignItems: "center",
+          marginTop: 20,
+          marginBottom: 100,
+          paddingBottom: 950, // ensures scroll doesnt cut off items
+        }}
+      >
+        {homeContent.map((content) => (
+          <View
+            key={content.id}
             style={{
-              fontSize: 20,
-              fontWeight: "bold",
-              marginBottom: 10,
-              textAlign: "center",
+              position: "relative",
+              width: "85%",
+              height: 180,
+              marginBottom: 100,
             }}
           >
-            Featured Item: Summer Berry Refresher
-          </ThemedText>
-          <Image
-            source={{
-              uri: "https://placehold.co/9BDD7F/FFFFFF?ThemedText=Featured+Item",
-            }}
-            style={{
-              width: "100%",
-              height: 180,
-              borderRadius: 10,
-              marginBottom: 15,
-              backgroundColor: "orange",
-            }}
-            contentFit="cover"
-          />
-          <ThemedText style={{ fontSize: 16, marginBottom: 10 }}>
-            Cool down with our Summer Berry Refresher! A delightful blend of
-            fresh berries and a hint of mint. Perfect for a sunny day.
-          </ThemedText>
-
-          <ThemedButton type="primary" title="Order Now" />
-        </ThemedView>
-
-        {/* coming soon */}
-        <ThemedView type="card">
-          <ThemedText
-            style={{
-              fontSize: 20,
-              fontWeight: "bold",
-              marginBottom: 10,
-            }}
-          >
-            Coming Soon: Spiced Pumpkin Latte
-          </ThemedText>
-          <Image
-            source={{
-              uri: "https://placehold.co/FFB74D/FFFFFF?ThemedText=Coming+Soon",
-            }}
-            style={{
-              width: "100%",
-              height: 180,
-              borderRadius: 10,
-              marginBottom: 15,
-              backgroundColor: "orange",
-            }}
-            contentFit="cover"
-          />
-          <ThemedText style={{ fontSize: 16, marginBottom: 10 }}>
-            Get ready for the cozy season with our new Spiced Pumpkin Latte! A
-            warm and comforting blend of pumpkin spice and rich espresso.
-          </ThemedText>
-        </ThemedView>
-
-        {/* about  */}
-        <ThemedView type="card">
-          <ThemedText
-            style={{
-              fontSize: 20,
-              fontWeight: "bold",
-              marginBottom: 10,
-            }}
-          >
-            About Us
-          </ThemedText>
-          <Image
-            source={{
-              uri: "https://placehold.co/FFB74D/FFFFFF?ThemedText=Coming+Soon",
-            }}
-            style={{
-              width: "100%",
-              height: 180,
-              borderRadius: 10,
-              marginBottom: 15,
-              backgroundColor: "orange",
-            }}
-            contentFit="cover"
-          />
-          <ThemedText style={{ fontSize: 16, marginBottom: 10 }}>
-            Welcome to Grabengo, your go-to app for quick and easy ordering.
-            Discover our mission and how were changing the way you enjoy your
-            favorite items!
-          </ThemedText>
-        </ThemedView>
+            <Image
+              source={{
+                uri: content.uri,
+              }}
+              style={{
+                width: "100%",
+                height: "100%",
+                borderRadius: 15,
+                backgroundColor: "orange",
+              }}
+            />
+            <ThemedText
+              style={{
+                position: "absolute",
+                bottom: 10,
+                left: 10,
+                fontSize: 20,
+                fontWeight: "bold",
+                paddingHorizontal: 8,
+                paddingVertical: 4,
+                borderRadius: 6,
+              }}
+            >
+              {content.header}
+            </ThemedText>
+            <ThemedText
+              style={{
+                fontSize: 14,
+                paddingHorizontal: 10,
+                paddingVertical: 5,
+                color: "rgba(252, 252, 252, 0.8)",
+              }}
+            >
+              {content.description}
+            </ThemedText>
+          </View>
+        ))}
       </ThemedScrollView>
     </ThemedView>
   );
