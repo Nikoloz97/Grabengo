c8a6b4dc42d70b1d8c0dd050b1005e5b0f0f65cd
Nikoloz97 <nick.gotsy@gmail.com>
2025-07-10 14:18:54 -0400

    recent orders modal now uses themed modal

--- components/account/recent-orders-modal.tsx
+++ components/account/recent-orders-modal.tsx
@@ -4,8 +4,8 @@ import { useTheme } from "@react-navigation/native";
 import { format } from "date-fns";
 import React from "react";
 import { Platform, ScrollView, TouchableOpacity, View } from "react-native";
-import Modal from "react-native-modal";
 import { ThemedButton } from "../themed-button";
+import { ThemedModal } from "../themed-modal";
 import { ThemedText } from "../themed-text";
 import { ThemedView } from "../themed-view";
 import { IconSymbol } from "../ui/IconSymbol";
@@ -28,178 +28,131 @@ export default function RecentOrdersModal({
   if (recentOrders.length === 0) {
     return (
       // TODO: make modal, view, (conditional) view elements a reusable wrapper
-      <Modal
-        isVisible={isVisible}
-        onBackdropPress={closeModal}
-        onBackButtonPress={closeModal}
-        onSwipeComplete={closeModal}
-        swipeDirection="down"
-        propagateSwipe={true}
-        style={{ justifyContent: "flex-end", margin: 0 }}
-      >
-        <View
-          style={{
-            backgroundColor: colors.background,
-            paddingTop: 20,
-            paddingBottom: 80,
-            borderTopLeftRadius: 20,
-            borderTopRightRadius: 20,
-            alignItems: "center",
-          }}
+      <ThemedModal isVisible={isVisible} onClose={closeModal}>
+        <ThemedView
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
         >
-          {/* Swipe indicator */}
-          <View
-            style={{
-              width: 50,
-              height: 5,
-              borderRadius: 2.5,
-              backgroundColor: colors.primary,
-            }}
-          />
-          <ThemedView
-            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-          >
-            <ThemedText>No past orders found :(</ThemedText>
-          </ThemedView>
-        </View>
-      </Modal>
+          <ThemedText>No past orders found :(</ThemedText>
+        </ThemedView>
+      </ThemedModal>
     );
   }
 
   return (
-    <Modal
+    <ThemedModal
       isVisible={isVisible}
-      onBackdropPress={closeModal}
-      onBackButtonPress={closeModal}
-      onSwipeComplete={closeModal}
-      propagateSwipe={true}
-      style={{ justifyContent: "flex-end", margin: 0 }}
+      onClose={closeModal}
+      showSwipeIndicator={false}
+      innerViewStyle={{ paddingHorizontal: 0, maxHeight: "80%" }}
     >
-      <View
-        style={{
-          backgroundColor: colors.background,
-          paddingTop: 20,
-          paddingBottom: 80,
-          borderTopLeftRadius: 20,
-          borderTopRightRadius: 20,
+      <ScrollView
+        style={{ width: "100%" }}
+        contentContainerStyle={{
           alignItems: "center",
-          flex: 1,
-          maxHeight: "80%",
-          width: "100%",
+          marginTop: 30,
+          paddingHorizontal: 30,
+          flexGrow: 1,
         }}
       >
-        <ScrollView
-          style={{ width: "100%" }}
-          contentContainerStyle={{
-            alignItems: "center",
-            marginTop: 30,
-            paddingHorizontal: 30,
-            flexGrow: 1,
-          }}
-        >
-          <ThemedText type="subtitle" style={{ marginBottom: 20 }}>
-            Recent Orders
-          </ThemedText>
-          {recentOrders.map((order, index) => (
-            <View key={index} style={{ width: "100%" }}>
-              <ThemedText style={{ color: colors.border }}>
-                {format(order.orderDate, "EEE, MMM d (h:mm aa)")}
-              </ThemedText>
-              <ThemedView
-                type="card"
+        <ThemedText type="subtitle" style={{ marginBottom: 20 }}>
+          Recent Orders
+        </ThemedText>
+        {recentOrders.map((order, index) => (
+          <View key={index} style={{ width: "100%" }}>
+            <ThemedText style={{ color: colors.border }}>
+              {format(order.orderDate, "EEE, MMM d (h:mm aa)")}
+            </ThemedText>
+            <ThemedView
+              type="card"
+              style={{
+                flexDirection: "column",
+                marginTop: 10,
+                borderRadius: 12,
+                position: "relative",
+                ...Platform.select({
+                  ios: {
+                    shadowColor: "rgb(0, 0, 0)",
+                    shadowOffset: { width: 0, height: 10 },
+                    shadowOpacity: 0.1,
+                    shadowRadius: 6,
+                  },
+                  android: {
+                    elevation: 4,
+                  },
+                }),
+              }}
+            >
+              {/* main content */}
+              <View
                 style={{
-                  flexDirection: "column",
-                  marginTop: 10,
-                  borderRadius: 12,
-                  position: "relative",
-                  ...Platform.select({
-                    ios: {
-                      shadowColor: "rgb(0, 0, 0)",
-                      shadowOffset: { width: 0, height: 10 },
-                      shadowOpacity: 0.1,
-                      shadowRadius: 6,
-                    },
-                    android: {
-                      elevation: 4,
-                    },
-                  }),
+                  alignItems: "flex-start",
+                  flex: 1,
                 }}
               >
-                {/* main content */}
-                <View
-                  style={{
-                    alignItems: "flex-start",
-                    flex: 1,
-                  }}
-                >
-                  <ThemedText style={{ color: colors.border }}>
-                    Items
-                  </ThemedText>
-                  <ThemedView style={{ flex: 1, marginTop: 5 }}>
-                    {order.items.map((item, index) => (
-                      <ThemedText
-                        key={index}
-                        numberOfLines={1}
-                        ellipsizeMode="tail"
-                      >
-                        {`•  ${item.quantity} - ${item.name}`}
-                      </ThemedText>
-                    ))}
-                  </ThemedView>
-                  <ThemedText style={{ color: colors.border, marginTop: 10 }}>
-                    Total
-                  </ThemedText>
-                  <ThemedText style={{ flex: 1, marginTop: 5 }}>
-                    {dollarFormatter(order.totalAmount)}
-                  </ThemedText>
-                  <ThemedText style={{ color: colors.border, marginTop: 10 }}>
-                    Status
-                  </ThemedText>
-                  <View style={{ flexDirection: "row" }}>
-                    <ThemedText style={{ flex: 1, marginTop: 5 }}>
-                      {order.status}
+                <ThemedText style={{ color: colors.border }}>Items</ThemedText>
+                <ThemedView style={{ flex: 1, marginTop: 5 }}>
+                  {order.items.map((item, index) => (
+                    <ThemedText
+                      key={index}
+                      numberOfLines={1}
+                      ellipsizeMode="tail"
+                    >
+                      {`•  ${item.quantity} - ${item.name}`}
                     </ThemedText>
-                    <View
+                  ))}
+                </ThemedView>
+                <ThemedText style={{ color: colors.border, marginTop: 10 }}>
+                  Total
+                </ThemedText>
+                <ThemedText style={{ flex: 1, marginTop: 5 }}>
+                  {dollarFormatter(order.totalAmount)}
+                </ThemedText>
+                <ThemedText style={{ color: colors.border, marginTop: 10 }}>
+                  Status
+                </ThemedText>
+                <View style={{ flexDirection: "row" }}>
+                  <ThemedText style={{ flex: 1, marginTop: 5 }}>
+                    {order.status}
+                  </ThemedText>
+                  <View
+                    style={{
+                      width: 40,
+                      height: 40,
+                      borderRadius: 20,
+                      borderColor: "rgba(49, 211, 20, 0.15)",
+                      backgroundColor: colors.primary,
+                    }}
+                  >
+                    <TouchableOpacity
+                      onPress={addToOrder}
                       style={{
-                        width: 40,
-                        height: 40,
+                        flex: 1,
+                        justifyContent: "center",
+                        alignItems: "center",
                         borderRadius: 20,
-                        borderColor: "rgba(49, 211, 20, 0.15)",
-                        backgroundColor: colors.primary,
                       }}
                     >
-                      <TouchableOpacity
-                        onPress={addToOrder}
-                        style={{
-                          flex: 1,
-                          justifyContent: "center",
-                          alignItems: "center",
-                          borderRadius: 20,
-                        }}
-                      >
-                        <IconSymbol size={20} name="plus" color="white" />
-                      </TouchableOpacity>
-                    </View>
+                      <IconSymbol size={20} name="plus" color="white" />
+                    </TouchableOpacity>
                   </View>
                 </View>
-              </ThemedView>
-            </View>
-          ))}
-
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "center",
-              gap: 30,
-              alignItems: "center",
-              marginTop: 20,
-              marginBottom: 30,
-            }}
-          >
-            <ThemedButton title="Close" type="primary" onPress={closeModal} />
+              </View>
+            </ThemedView>
           </View>
-        </ScrollView>
-      </View>
-    </Modal>
+        ))}
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            gap: 30,
+            alignItems: "center",
+            marginTop: 20,
+            marginBottom: 30,
+          }}
+        >
+          <ThemedButton title="Close" type="primary" onPress={closeModal} />
+        </View>
+      </ScrollView>
+    </ThemedModal>
   );
 }
