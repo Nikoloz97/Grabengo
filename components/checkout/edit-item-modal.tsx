a2b78cd96b9ad70981d210e01363728a75e24c79
Nikoloz97 <nick.gotsy@gmail.com>
2025-07-10 14:12:10 -0400

    edit item modal now uses themed modal

--- components/checkout/edit-item-modal.tsx
+++ components/checkout/edit-item-modal.tsx
@@ -5,8 +5,8 @@ import { useTheme } from "@react-navigation/native";
 import { Image } from "expo-image";
 import React, { useEffect, useState } from "react";
 import { ScrollView, TouchableOpacity, View } from "react-native";
-import Modal from "react-native-modal";
 import { ThemedButton } from "../themed-button";
+import { ThemedModal } from "../themed-modal";
 import { ThemedText } from "../themed-text";
 
 interface EditItemModalProps {
@@ -43,144 +43,104 @@ export default function EditItemModal({
   };
 
   return (
-    <Modal
-      isVisible={isVisible}
-      onBackdropPress={resetModal}
-      onBackButtonPress={resetModal}
-      onSwipeComplete={resetModal}
-      swipeDirection="down"
-      propagateSwipe={true}
-      style={{ justifyContent: "flex-end", margin: 0 }}
-    >
-      <View
-        style={{
-          backgroundColor: colors.background,
-          paddingTop: 20,
-          paddingBottom: 80,
-          borderTopLeftRadius: 20,
-          borderTopRightRadius: 20,
-          alignItems: "center",
-        }}
+    <ThemedModal isVisible={isVisible} onClose={resetModal}>
+      <ScrollView
+        contentContainerStyle={{ alignItems: "center", marginTop: 30 }}
       >
-        {/* Swipe indicator */}
+        {/* Image */}
+        <Image
+          source={{ uri: item.image }}
+          style={{
+            width: 120,
+            height: 120,
+            borderRadius: 60,
+            backgroundColor: "orange",
+          }}
+          contentFit="cover"
+        />
+        <ThemedText
+          type="subtitle"
+          style={{
+            marginTop: 10,
+            paddingHorizontal: 10,
+            alignItems: "center",
+            textAlign: "center",
+          }}
+        >
+          {item.name}
+        </ThemedText>
+        <ThemedText style={{ color: "#999", marginVertical: 5 }}>
+          {item.calories} calories • {item.protein}g protein • $
+          {dollarFormatter(item.price)}
+        </ThemedText>
+        {/* Description */}
+        <ThemedText
+          style={{
+            fontSize: 14,
+            color: "#777",
+            textAlign: "center",
+            marginTop: 10,
+            marginBottom: 20,
+            marginHorizontal: 20,
+          }}
+        >
+          {item.description}
+        </ThemedText>
+        {/* Quantity + Add Button Row */}
         <View
           style={{
-            width: 50,
-            height: 5,
-            borderRadius: 2.5,
-            backgroundColor: colors.primary,
+            flexDirection: "row",
+            justifyContent: "center",
+            gap: 30,
+            alignItems: "center",
+            marginBottom: 30,
           }}
-        />
-
-        <ScrollView
-          contentContainerStyle={{ alignItems: "center", marginTop: 30 }}
         >
-          {/* Image */}
-          <Image
-            source={{ uri: item.image }}
-            style={{
-              width: 120,
-              height: 120,
-              borderRadius: 60,
-              backgroundColor: "orange",
-            }}
-            contentFit="cover"
-          />
-
-          <ThemedText
-            type="subtitle"
-            style={{
-              marginTop: 10,
-              paddingHorizontal: 10,
-              alignItems: "center",
-              textAlign: "center",
-            }}
-          >
-            {item.name}
-          </ThemedText>
-
-          <ThemedText style={{ color: "#999", marginVertical: 5 }}>
-            {item.calories} calories • {item.protein}g protein • $
-            {dollarFormatter(item.price)}
-          </ThemedText>
-
-          {/* Description */}
-          <ThemedText
-            style={{
-              fontSize: 14,
-              color: "#777",
-              textAlign: "center",
-              marginTop: 10,
-              marginBottom: 20,
-              marginHorizontal: 20,
-            }}
-          >
-            {item.description}
-          </ThemedText>
-
-          {/* Quantity + Add Button Row */}
           <View
             style={{
               flexDirection: "row",
-              justifyContent: "center",
-              gap: 30,
               alignItems: "center",
-              marginBottom: 30,
             }}
           >
-            <View
+            <TouchableOpacity
+              onPress={() => setQuantity((prev) => Math.max(1, prev - 1))}
+              disabled={quantity === 1}
               style={{
-                flexDirection: "row",
-                alignItems: "center",
+                paddingHorizontal: 12,
+                paddingVertical: 6,
+                borderRadius: 5,
+                backgroundColor: quantity === 1 ? "#ccc" : colors.primary,
+                marginRight: 8,
               }}
             >
-              <TouchableOpacity
-                onPress={() => setQuantity((prev) => Math.max(1, prev - 1))}
-                disabled={quantity === 1}
-                style={{
-                  paddingHorizontal: 12,
-                  paddingVertical: 6,
-                  borderRadius: 5,
-                  backgroundColor: quantity === 1 ? "#ccc" : colors.primary,
-                  marginRight: 8,
-                }}
-              >
-                <ThemedText style={{ color: "#fff", fontSize: 18 }}>
-                  −
-                </ThemedText>
-              </TouchableOpacity>
-
-              <ThemedText style={{ fontSize: 16, marginHorizontal: 4 }}>
-                {quantity}
-              </ThemedText>
-
-              <TouchableOpacity
-                onPress={() => setQuantity((prev) => prev + 1)}
-                style={{
-                  paddingHorizontal: 12,
-                  paddingVertical: 6,
-                  borderRadius: 5,
-                  backgroundColor: colors.primary,
-                  marginLeft: 8,
-                }}
-              >
-                <ThemedText style={{ color: "#fff", fontSize: 18 }}>
-                  +
-                </ThemedText>
-              </TouchableOpacity>
-            </View>
-
-            {/* Add to Order Button */}
-            <ThemedButton
-              title="Edit Order"
-              type="primary"
-              onPress={() => {
-                editOrder(item.id, quantity);
+              <ThemedText style={{ color: "#fff", fontSize: 18 }}>−</ThemedText>
+            </TouchableOpacity>
+            <ThemedText style={{ fontSize: 16, marginHorizontal: 4 }}>
+              {quantity}
+            </ThemedText>
+            <TouchableOpacity
+              onPress={() => setQuantity((prev) => prev + 1)}
+              style={{
+                paddingHorizontal: 12,
+                paddingVertical: 6,
+                borderRadius: 5,
+                backgroundColor: colors.primary,
+                marginLeft: 8,
               }}
-            />
+            >
+              <ThemedText style={{ color: "#fff", fontSize: 18 }}>+</ThemedText>
+            </TouchableOpacity>
           </View>
-        </ScrollView>
-      </View>
-    </Modal>
+          {/* Add to Order Button */}
+          <ThemedButton
+            title="Edit Order"
+            type="primary"
+            onPress={() => {
+              editOrder(item.id, quantity);
+            }}
+          />
+        </View>
+      </ScrollView>
+    </ThemedModal>
   );
 }
